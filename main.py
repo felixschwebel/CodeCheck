7b3140aecb84f441a60c87cc4be250a111b52eb1
felixschwebel <schwebel.felix@gmail.com>
2023-01-28 19:13:54 +0100

    login and import statements

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 import os
 
+# ------- Flask App -------
 app = Flask(__name__)
 bootstrap = Bootstrap5()
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route("/")
