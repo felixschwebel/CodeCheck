583a981d03444c515af07098e3a41885fa40b880
felixschwebel <schwebel.felix@gmail.com>
2023-02-18 09:18:27 +0100

    codecheck cleanup, comments

--- server.py
+++ server.py
@@ -8,31 +8,36 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 import os
 
-
-# ------- FlaskForms -------
-class CodeForm(FlaskForm):
-    source_code = CodeMirrorField(language='python', config={'lineNumbers': 'true'})
-    submit = SubmitField('Submit')
-
-# mandatory
-CODEMIRROR_LANGUAGES = ['python', 'yaml', 'htmlembedded']
+# ---------------------------- CONFIGURATIONS ------------------------
+# CodeMirror Settings
+CODEMIRROR_LANGUAGES = ['python', 'java', 'C']
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'secret'
+SECRET_KEY = os.environ['SECRET_KEY']
 CODEMIRROR_THEME = 'darcula'
 CODEMIRROR_ADDONS = (('ADDON_DIR', 'ADDON_NAME'),)
 
-# ------- Flask App -------
-app = Flask(__name__)
-bootstrap = Bootstrap5()
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-app.config.from_object(__name__)
-codemirror = CodeMirror(app)
-
-
+# LoginManager
 # login_manager = LoginManager()
 # login_manager.init_app(app)
 
 
+# ---------------------------- FLASK-FORMS ----------------------------
+class CodeForm(FlaskForm):
+    source_code = CodeMirrorField(language='python', config={'lineNumbers': 'true'})
+    submit = SubmitField('Submit')
+
+
+# ---------------------------- FLASK APP ------------------------------
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config.from_object(__name__)
+
+# App Components
+bootstrap = Bootstrap5(app)
+codemirror = CodeMirror(app)
+
+
+# ---------------------------- ROUTING --------------------------------
 @app.route("/")
 def home():
     return render_template('account.html')
@@ -47,5 +52,6 @@ def codecheck():
     return render_template('codecheck.html', code_form=code_form)
 
 
+# ---------------------------- RUN SERVER ------------------------------
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,10 +1,9 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+/* Side navigation */
 function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
   document.getElementById("main").style.marginRight = "300px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginRight = "0";
